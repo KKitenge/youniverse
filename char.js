4e8bc070b7cd0fcc98521cfab0764a08d41e27d1
Ox Noj <geneveva@gmail.com>
2023-05-11 09:49:40 -0700

    Updated character css and js

--- char.js
+++ char.js
@@ -2,19 +2,19 @@ let container = document.querySelector(".container")
 let loadMoreBtn = document.querySelector("#load_more")
 
 // Fetch data from json file
-async function fetchCharacters(){
-  const res =  await fetch("characters.json")
+async function fetchCharacters() {
+  const res = await fetch("characters.json")
   const data = await res.json()
- 
-      // Create a card for each character
-      data.splice(0,6).forEach(character => {
-        //let x = 0
-        //x += 5 // x = x(old) + 5
-        container.innerHTML += `
+
+  // Create a card for each character
+  data.splice(0, 6).forEach(character => {
+    //let x = 0
+    //x += 5 // x = x(old) + 5
+    container.innerHTML += `
         <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${character.images.md}" alt="${ character.name}">
+            <img class="card-img-top" src="${character.images.md}" alt="${character.name}">
             <div class="card-body">
-                <h5 class="card-name">${ character.name}</h5>
+                <h5 class="card-name">${character.name}</h5>
                 <p class="card-fullName">${character.biography.fullName}</p>
                 <p class="card-placeOfBirth">${character.biography.placeOfBirth}</p>
                 <p class="card-race">${character.appearance.race}</p>
@@ -28,19 +28,21 @@ async function fetchCharacters(){
 fetchCharacters()
 
 loadMoreBtn.addEventListener("click", () => {
-  
+
 })
 
-  // Autocomplete bar
-    //Load names from json file, too hard otherwise
-  $(function() {
-    $.getJSON("characters.json", function(availableCharacters) {
-      $("#characters").autocomplete({
-        source: availableCharacters
-      });
+// Autocomplete bar
+//Load names from json file, too hard otherwise
+$(function () {
+  $.getJSON("characters.json", function (characters) {
+    $("#characters").autocomplete({
+      source: characters.map(function (character) {
+        return character.name;
+      })
     });
   });
-  
+});
+
 //   $(function() {
 //     var availableCharacters = [
 //     ];
@@ -48,4 +50,3 @@ loadMoreBtn.addEventListener("click", () => {
 //       source: availableCharacters
 //     });
 //   });
-  

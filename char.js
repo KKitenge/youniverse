78924c40a11c4a37f37eaf2d5a126ce566cb095f
Ox Noj <geneveva@gmail.com>
2023-05-10 20:05:32 -0700

    edited character html/char.js - all characters show

--- char.js
+++ char.js
@@ -5,19 +5,25 @@ fetch('characters.json')
     // Get the card element
     const card = document.querySelector('.card');
 
-    // Create a card for each character - to be changed so that character card
-        //appears for searched characters only, not appearing by default
+    // Create a card for each character
     data.forEach(character => {
-    
-      // Set the image source
+      // Clone the card element for each character
+        //clone gets all characters/cards but this needs to be limited
+      const newCard = card.cloneNode(true);
+
+      // Set the image source 
       newCard.querySelector('.card-img-top').src = character.images.md;
 
       // The character description/text
       newCard.querySelector('.card-name').textContent = character.name;
+      newCard.querySelector('.card-fullName').textContent = character.biography.fullName;
+      newCard.querySelector('.card-placeOfBirth').textContent = character.biography.placeOfBirth;
+      newCard.querySelector('.card-race').textContent = character.appearance.race;
+      newCard.querySelector('.card-gender').textContent = character.appearance.gender;
+      newCard.querySelector('.card-alignment').textContent = character.biography.alignment.toUpperCase();
 
       // Add the new card to the page
       document.body.appendChild(newCard);
     });
   })
   .catch(error => console.error(error));
-

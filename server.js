159ad8ceef801e01ae06c41a115e978b6c016c4b
Jordan Cardenas <jordancardenas@Jordans-MBP.attlocal.net>
2023-05-11 19:43:32 -0700

    fix errors

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+const { User } = require('./models');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -43,7 +45,7 @@ app.post('/signup', async (req, res) => {
 
     console.log(newUser);
 
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(200).json({ message: 'User created successfully' });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: 'Failed to create user' });

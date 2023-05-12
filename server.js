c98834d51227b5a71742d9f6591933f0c7824d8d
Ox Noj <geneveva@gmail.com>
2023-05-12 15:35:10 -0700

    edited handlebars and it works

--- server.js
+++ server.js
@@ -1,55 +1,117 @@
-const path = require('path');
-const express = require('express');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
-const { User } = require('./models');
+// const path = require('path');
+// const express = require('express');
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
+// const sequelize = require('./config/connection');
+// const { User } = require('./models');
 
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// const exphbs = require('express-handlebars');
+// const hbs = exphbs.create({ helpers });
+
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
+
+// const session = require('express-session');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// const sess = {
+//   secret: 'Super secret secret',
+//   cookie: {},
+//   resave: true,
+//   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   }),
+// };
+
+// app.use(session(sess));
+
+// // Signup route
+// app.post('/signup', async (req, res) => {
+//   try {
+//     const newUser = await User.create({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
+
+//     console.log(newUser);
+
+//     res.status(200).json({ message: 'User created successfully' });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ message: 'Failed to create user' });
+//   }
+// });
+
+// // Routes
+// app.use(routes);
+
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+// });
+
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+const sequelize = require("./config/connection");
+const { User } = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {},
   resave: true,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
+    db: sequelize,
   }),
 };
 
 app.use(session(sess));
 
 // Signup route
-app.post('/signup', async (req, res) => { 
+app.post("/signup", async (req, res) => {
   try {
     const newUser = await User.create({
       username: req.body.username,
       email: req.body.email,
       password: req.body.password,
     });
-
+    
     console.log(newUser);
-
-    res.status(200).json({ message: 'User created successfully' });
+    res.status(200).json({ message: "User created successfully" });
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: 'Failed to create user' });
+    res.status(500).json({ message: "Failed to create user" });
   }
+});
+
+// Route to serve characters.json
+app.get("/characters", (req, res) => {
+  res.sendFile(path.join(__dirname, "characters.json"));
 });
 
 // Routes

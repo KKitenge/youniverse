723ce53676f65cfb029a3a88fe6ea6640f7425ba
Ox Noj <geneveva@gmail.com>
2023-05-12 15:01:35 -0700

    editing character/dashboard handlebar and char.js

--- char.js
+++ char.js
@@ -1,10 +1,13 @@
-let container = document.querySelector(".container")
-let loadMoreBtn = document.querySelector("#load_more")
+let container = document.querySelector(".container");
+let loadMoreBtn = document.querySelector("#load_more");
+let currentCardIndex = 0;
+let cardsPerPage = 3;
+let data = [];
 
 // Fetch data from json file
 async function fetchCharacters() {
   const res = await fetch("characters.json")
-  const data = await res.json()
+  data = await res.json()
 
   // Create a card for each character
   data.splice(0, 6).forEach(character => {
@@ -20,7 +23,6 @@ async function fetchCharacters() {
                 <p class="card-race">${character.appearance.race}</p>
                 <p class="card-gender">${character.appearance.gender}</p>
                 <p class="card-alignment">${character.biography.alignment.toUpperCase()}</p>
-                <button class="favorite-button">Add to favorites</button>
             </div>
         </div>
         `})
@@ -28,8 +30,34 @@ async function fetchCharacters() {
 fetchCharacters()
 
 loadMoreBtn.addEventListener("click", () => {
-
-})
+  for (let i = 0; i < cardsPerPage; i++) {
+    if (currentCardIndex >= data.length) {
+      loadMoreBtn.style.display = "none";
+      break;
+    }
+    container.innerHTML += `
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${
+          data[currentCardIndex].images.md
+        }" alt="${data[currentCardIndex].name}">
+        <div class="card-body">
+          <h5 class="card-name">${data[currentCardIndex].name}</h5>
+          <p class="card-fullName">${
+            data[currentCardIndex].biography.fullName
+          }</p>
+          <p class="card-placeOfBirth">${
+            data[currentCardIndex].biography.placeOfBirth
+          }</p>
+          <p class="card-race">${data[currentCardIndex].appearance.race}</p>
+          <p class="card-gender">${data[currentCardIndex].appearance.gender}</p>
+          <p class="card-alignment">${data[
+            currentCardIndex
+          ].biography.alignment.toUpperCase()}</p>
+        </div>
+      </div>`;
+    currentCardIndex++;
+  }
+});
 
 // Autocomplete bar
 //Load names from json file, too hard otherwise
